0586464a4ceb876663ab968159aeddd5c9f5b366
Cole Webb <cole.webb@outlook.com>
2021-12-02 20:45:39 -0700

    got seed selection mostly working

--- segment.py
+++ segment.py
@@ -28,26 +28,8 @@ class segment():
     # Takes the location of the center of the area of interest
     # Returns a float
     def stdDev(self, rowLocation, columnLocation) -> float:
-        if (rowLocation - 1 < 0) or (rowLocation + 1 > self.rows):                                                  # if we're on an edge
-            return 0.0                                                                                              # return zero
-        if (columnLocation - 1 < 0) or (columnLocation + 1 > self.columns):                                         #
-            return 0.0                                                                                              #
-        yStdDev = 0                                                                                                 # set initial standard deviations for all three channels
-        cbStdDev = 0                                                                                                #
-        crStdDev = 0                                                                                                #
-        yMean = np.mean(self.image[columnLocation - 1:columnLocation + 1, rowLocation - 1:rowLocation + 1, 0])      # calculate area means for all three channels
-        cbMean = np.mean(self.image[columnLocation - 1:columnLocation + 1, rowLocation - 1:rowLocation + 1, 1])     #
-        crMean = np.mean(self.image[columnLocation - 1:columnLocation + 1, rowLocation - 1:rowLocation + 1, 2])     #
-        for row in range(rowLocation - 1, rowLocation + 1):                                                         # for each row in the neighborhood
-            for column in range(columnLocation - 1, columnLocation + 1):                                            # for each column in the neighborhood
-                yStdDev += (self.image[column, row] - yMean)                                                        # update standard deviation sums
-                cbStdDev += (self.image[column, row] - cbMean)                                                      #
-                crStdDev += (self.image[column, row] - crMean)                                                      #
-        yStdDev = np.sqrt(yStdDev * (1/9))                                                                          # divide by 9 and take the square root
-        cbStdDev = np.sqrt(cbStdDev * (1/9))                                                                        #
-        crStdDev = np.sqrt(crStdDev * (1/9))                                                                        #
-        stdDev = yStdDev + cbStdDev + crStdDev                                                                      # sum
-        return stdDev                                                                                               # return
+        neighborhood = self.image[columnLocation - 1:columnLocation + 1, rowLocation - 1:rowLocation + 1]   # grab contents of neighborhood
+        return np.std(neighborhood)                                                                         # return standard deviation of neighborhood
 
     # Calculates the euclidean distance from a pixel to its eight neighbors
     # Takes the location of interest in the original image
@@ -63,15 +45,9 @@ class segment():
                             self.image[columnLocation - 1, rowLocation - 1],    #
                             self.image[columnLocation, rowLocation - 1],        #
                             self.image[columnLocation + 1, rowLocation - 1]]    #
-        for i in range(0, 8):
-            # The below equation is messy. Here's a nicer version:
-            #
-            #            sqrt(((pixel[0] - center[0])^2) + ((pixel[1] - center[1])^2) + ((pixel[2] - center[2])^2))
-            # distance = ------------------------------------------------------------------------------------------
-            #                                 sqrt(pixel[0]^2 + pixel[1]^2 + pixel[2]^2)
-            #
-            distances[i] = np.sqrt(((neighborhood[i, 0] - center[0])^2) + ((neighborhood[i, 1] - center[1])^2) + ((neighborhood[i, 2] - center[2])^2))/np.sqrt(neighborhood[i, 0]^2 + neighborhood[i, 1]^2 + neighborhood[i, 2]^2)
-        return max(distances)
+        for i in range(0, 8):                                                   # for each pixel in the neighborhood (except the center)
+            distances[i] = np.linalg.norm(neighborhood[i] - center)             # calculate distance between center and pixel
+        return max(distances)                                                   # return max distance
 
     # Calculates initial seed pixels for the entire image
     # Uses the given image
@@ -81,7 +57,7 @@ class segment():
             for column in range(1, self.columns - 1):                                   #
                 self.stdDevs[column, row] = self.stdDev(row, column)                    # calculate standard deviations
                 self.distances[column, row] = self.euclideanDistance(row, column)       # calculate distances
-        self.stdDevs = self.stdDevs/max(self.stdDevs)                                   # normalize standard deviations
+        self.stdDevs = self.stdDevs/np.amax(self.stdDevs)                               # normalize standard deviations
         thresholdedStdDevs = cv.threshold(self.stdDevs, 0, 1, cv.THRESH_OTSU)           # threshold standard deviations
         thresholdedDistances = cv.threshold(self.distances, 0.05, 1, cv.THRESH_BINARY)  # check conditions
         self.seededPixels = thresholdedStdDevs & thresholdedDistances                   # generate seeded pixels mask and save
@@ -95,7 +71,7 @@ class segment():
 
     def main(self) -> None:
         self.imageConvert()     # Step 1: Complete
-        self.seedSelection()    # Step 2: Complete (needs testing)
+        self.seedSelection()    # Step 2: Complete (except for picking seed pixels, needs debugging)
         self.regionGrowing()    # Step 3: Planned
         self.regionMerging()    # Step 4: Planned
 
